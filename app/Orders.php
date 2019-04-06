d7210c2382a210e2ed8592e08da271ad1523a523
AroicX <Arowosegbe67@gmail.com>
2019-04-06 11:40:45 -0800

    Created Relationships

--- app/Orders.php
+++ app/Orders.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Products;
 use Illuminate\Database\Eloquent\Model;
 
 class Orders extends Model
 {
-    //
+  
+  public function Product()
+  {
+     return $this->belongsTo(Products::class);
+  }
+
 }
--- app/Products.php
+++ app/Products.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Orders;
 use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+   public function Orders()
+   {
+      return $this->hasMany(Orders::class);
+   }
 }
